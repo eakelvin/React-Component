e0f2104b50dce0fb144372a16929206853326cd9
Kelvin Akaba <akabakelvin@gmail.com>
2023-03-18 09:01:58 +0000

    React Component

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App(props) {
+  return (
+        
+        <div className = "container">
+        <img className="qr" src="./qrcode.jpg" />
+        <h1>Improve your front-end <br></br> skills by building projects</h1>
+        <p>Scan the QR code to visit Frontend <br></br>
+            Mentor and take your coding skills to <br></br>
+            the next level</p>
+        </div> 
+  );
+}
+
+export default App;
+
